e3e58442a68165e725e97be5f94d7694a8b38e4a
Valentin Peres <valentin.peres@widergy.com>
2026-09-23 12:21:51 -0300

    feat: [TR-1497] add notes seeds for north and south utilities

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,13 +22,72 @@ FactoryBot.create_list(:user, 20, utility: north_utility,
 FactoryBot.create_list(:user, 20, utility: south_utility,
                                   password: '12345678', password_confirmation: '12345678')
 
-FactoryBot.create(:user, utility: south_utility, email: 'test_south@widergy.com',
-                         password: '12345678', password_confirmation: '12345678')
+south_user = FactoryBot.create(
+  :user,
+  utility: south_utility,
+  email: 'test_south@widergy.com',
+  password: '12345678',
+  password_confirmation: '12345678'
+)
 
-FactoryBot.create(:user, utility: north_utility, email: 'test_north@widergy.com',
-                         password: '12345678', password_confirmation: '12345678')
+north_user = FactoryBot.create(
+  :user,
+  utility: north_utility,
+  email: 'test_north@widergy.com',
+  password: '12345678',
+  password_confirmation: '12345678'
+)
 
 User.all.find_each do |user|
   random_books_amount = [1, 2, 3].sample
-  FactoryBot.create_list(:book, random_books_amount, user: user, utility: user.utility)
+  FactoryBot.create_list(
+    :book,
+    random_books_amount,
+    user: user,
+    utility: user.utility
+  )
 end
+
+# Notes - North Utility
+Note.create!(
+  title: 'North short review',
+  content: 'palabra ' * 40,
+  note_type: :review,
+  user: north_user
+)
+
+Note.create!(
+  title: 'North medium critique',
+  content: 'palabra ' * 80,
+  note_type: :critique,
+  user: north_user
+)
+
+Note.create!(
+  title: 'North long critique',
+  content: 'palabra ' * 110,
+  note_type: :critique,
+  user: north_user
+)
+
+# Notes - South Utility
+Note.create!(
+  title: 'South short review',
+  content: 'palabra ' * 50,
+  note_type: :review,
+  user: south_user
+)
+
+Note.create!(
+  title: 'South medium critique',
+  content: 'palabra ' * 90,
+  note_type: :critique,
+  user: south_user
+)
+
+Note.create!(
+  title: 'South long critique',
+  content: 'palabra ' * 130,
+  note_type: :critique,
+  user: south_user
+)

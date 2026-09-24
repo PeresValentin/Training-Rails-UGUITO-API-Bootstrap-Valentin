aed5a05dbba8a4d3c3c5a7faa8f3f78c4e6ed371
Valentin Peres <valentin.peres@widergy.com>
2026-09-24 16:17:50 -0300

    feat: [TR-1498] add note enum specs

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -12,6 +12,14 @@ RSpec.describe Note, type: :model do
   it { is_expected.to belong_to(:user) }
   it { is_expected.to have_one(:utility).through(:user) }
 
+  it do
+  is_expected.to define_enum_for(:note_type)
+    .with_values(review: 0, critique: 1)
+  end
+  it 'rejects an invalid note type' do
+  expect { build(:note, note_type: :invalid) }.to raise_error(ArgumentError)
+  end
+
   it 'has a valid factory' do
     expect(subject).to be_valid
   end

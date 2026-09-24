d482ed93ee8f3f5bd233cbd8069a66ac4d7c404c
Valentin Peres <valentin.peres@widergy.com>
2026-09-24 15:58:24 -0300

    feat: [TR-1498] add note validations and associations specs

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -5,6 +5,13 @@ RSpec.describe Note, type: :model do
     build(:note)
   end
 
+  %i[title content note_type].each do |value|
+    it { is_expected.to validate_presence_of(value) }
+  end
+
+  it { is_expected.to belong_to(:user) }
+  it { is_expected.to have_one(:utility).through(:user) }
+
   it 'has a valid factory' do
     expect(subject).to be_valid
   end
